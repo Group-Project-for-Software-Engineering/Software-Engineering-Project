cf518a27e8cd9b590a74a3ece5a8a2afca184329
Tristan Huertas <tristan.huertas22@my.stjohns.edu>
2026-04-21 10:22:02 -0400

    Made description for Request class

--- frontend/classes/Request.java
+++ frontend/classes/Request.java
@@ -1,3 +1,10 @@
+/*  Project: Vehicular Cloud Real Time System (VCRTS)
+ * Class: Request.java
+ * Authors: Group 2 (Justin Cracchiolo, Lauren Rodriguez, David Choi, Tristan Huertas, Ivan Lin, Anthony Vallejo, Sebastian Villavicencio)
+ * Date: March 2026
+ * This program represents a request in the VCRTS system. 
+ * It contains the type of request (job or vehicle), the output stream to send responses back to the requester, and the decision made regarding the request.
+ */
 package classes;
 
 import java.io.DataOutputStream;
